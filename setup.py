5e9346ff1eaf35d5234aeb574f152f87faf566d3
Iveta Champoeva <ivi5317@gmail.com>
2016-05-25 00:23:52 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Hotel Reservations",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['nothing so far'],
+    author="Iveta Champoeva",
+    author_email="ivi5317@gmail.com",
+    description="System for hotel reservations",
+    license="MIT",
+    keywords="python hotel reservation",
+    url="http://example.com/hello_world/"
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: Microsoft",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Database :: Front-Ends"
+    ]
+)
