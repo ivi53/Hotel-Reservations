54229376f6cf683e1b2705fb1cacc312b36b66d9
Iveta Champoeva <ivi5317@gmail.com>
2016-05-25 17:46:44 +0300

    Update rooms_database.py

--- rooms_database.py
+++ rooms_database.py
@@ -1,1 +1,56 @@
 import sqlite3
+
+def create_comforts():
+    pass
+
+def add_comforts(*comforts):
+    pass
+
+def get_comforts(room):
+    pass
+
+def create_capacity():
+    pass
+
+def add_capacity(capacity):
+    pass
+
+def get_capacity(room):
+    pass
+
+def create_floors():
+    pass
+
+def add_floor(number):
+    pass
+
+def get_floor(room):
+    pass
+
+def create_possible_problems():
+    pass
+
+def add_problem(*problems):
+    pass
+
+def get_problem(room):
+    pass
+
+def create_room():
+    '''adding somehow the comforts, the floor, the capacity,
+    the problems and availability'''
+
+def add_room(number):
+    pass
+
+def get_room(number):
+    pass
+
+def set_availability(room):
+    pass
+
+def set_comforts(room):
+    pass
+
+def set_problems(room):
+    pass
